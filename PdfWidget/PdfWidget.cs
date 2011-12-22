613747eb7ca079a3273dc6dc3903c2f334229412
Peter Gill <peter@majorsilence.com>
2011-12-21 22:16:48 -0330

    New buttons to go to first or last page.

--- PdfWidget/PdfWidget.cs
+++ PdfWidget/PdfWidget.cs
@@ -1,5 +1,10 @@
 using System;
 
+// The Gtk developers wanted icons not to appear on buttons. 
+// On Linux, this can be changed by editing the gconf key
+// /desktop/gnome/interface/buttons_have_icons
+
+// type page number /OfPageCount, 
 
 namespace PdfWidget
 {
@@ -9,7 +14,8 @@ namespace PdfWidget
 
 		private Poppler.Document pdf;
 		private int pageIndex = 0;
-					
+		private int pageHeight = 0;
+		
 		public PdfWidget ()
 		{
 			this.Build ();
@@ -22,6 +28,7 @@ namespace PdfWidget
 			double width=0D;
 			double height=0D;
 			page.GetSize(out width, out height);
+			pageHeight = (int)height;
 			
 			// It is important to set the image to have the correct size
 			img.Pixbuf  = new  Gdk.Pixbuf (Gdk.Colorspace.Rgb, false, 8, (int)width, (int)height);
@@ -34,7 +41,10 @@ namespace PdfWidget
 		
 		public void LoadPdf(string pdfFileName)
 		{
-			pdf = Poppler.Document.NewFromFile(pdfFileName, "");	
+			pdf = Poppler.Document.NewFromFile(pdfFileName, "");
+			PageCountLabel.Text = @"/" + pdf.NPages.ToString();	
+			CurrentPage.Value = 0;
+			CurrentPage.Adjustment.Upper = pdf.NPages;
 			SetContinuousPageMode();
 		}
 		
@@ -126,6 +136,17 @@ namespace PdfWidget
 			RenderPage(ref this.image1);
 		}
 
+		protected void OnFirstPageButtonClicked (object sender, System.EventArgs e)
+		{
+			scrolledwindow1.Hadjustment.Value = scrolledwindow1.Hadjustment.Lower;
+			scrolledwindow1.Vadjustment.Value = scrolledwindow1.Vadjustment.Lower;	
+		}
+
+		protected void OnLastPageButtonClicked (object sender, System.EventArgs e)
+		{
+			scrolledwindow1.Hadjustment.Value = scrolledwindow1.Hadjustment.Lower;
+			scrolledwindow1.Vadjustment.Value = scrolledwindow1.Vadjustment.Upper-pageHeight;	
+		}
 	}
 }
 
